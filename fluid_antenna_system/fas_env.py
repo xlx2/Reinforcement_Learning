8762f7fdf1764d73a7b5c7319d6238e90de3bc61
xulvxin <751333411@qq.com>
2024-12-24 23:13:18 +0800

    add

--- fluid_antenna_system/fas_env.py
+++ fluid_antenna_system/fas_env.py
@@ -5,27 +5,37 @@ import numpy as np
 class FluidAntennaSystemEnv(gym.Env):
     def __init__(self, parameters: dict):
         super().__init__()
-        self.num_of_selected_antennas = parameters["num_of_selected_antennas"]
         self.num_of_antennas = parameters["num_of_antennas"]
         self.num_of_users = parameters["num_of_users"]
 
-        self.action_space = gym.spaces.Box(
-            low=0,
-            high=1,
-            shape=(self.num_of_antennas, self.num_of_users),
-            dtype=np.float32
-        )
-        self.observation_space = gym.spaces.Box(
-            low=-np.inf,
-            high=np.inf,
-            shape=(parameters["channel"].shape[0], parameters["channel"].shape[1], 2),
-            dtype=np.float32
-        )
+        self.action_space = gym.spaces.MultiBinary((
+            self.num_of_antennas, self.num_of_users + self.num_of_antennas
+        ))
+        self.observation_space = gym.spaces.Tuple((
+            gym.spaces.Box(
+                low=-np.inf,
+                high=np.inf,
+                shape=(parameters["channel"].size * 2,),
+                dtype=np.float32
+            ),
+            gym.spaces.Box(
+                low=-90,
+                high=90,
+                shape=(parameters["doa"].size,),
+                dtype=np.float32
+            )
+        ))
 
-    def step(self, action) -> tuple[np.ndarray, np.float32, bool, bool, {}]:
+    def step(self, action) -> tuple[tuple[np.ndarray, np.ndarray], np.float32, bool, bool, {}]:
 
         return observations, rewards, terminated, truncated, {}
 
-    def reset(self,) -> tuple[np.ndarray, {}]:
+    def reset(self, seed, options) -> tuple[tuple[np.ndarray, np.ndarray], {}]:
 
         return observations, {}
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
